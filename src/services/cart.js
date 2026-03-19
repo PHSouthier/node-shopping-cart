824241165ed42a97ab95d2bdbb8902993df70c9c
Pedro_Southier <phsouthier@hotmail.com>
2026-03-19 19:43:04 -0300

    criado funcao para calcular total do carrinho

--- src/services/cart.js
+++ src/services/cart.js
@@ -2,3 +2,11 @@
 async function addItem(userCart, item) {
   userCart.push(item);
 }
+
+async function calculateTotal(userCart) {
+  console.log("\nShopping Cart TOTAL IS:");
+
+  const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
+  console.log(`🎁Total: ${result}`);
+}
+export { addItem, calculateTotal, deleteItem, removeItem, displaycart };

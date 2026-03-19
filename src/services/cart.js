303bc2e184c17ff50598dc2687ad2076f803daa8
Pedro_Southier <phsouthier@hotmail.com>
2026-03-19 19:43:20 -0300

    criado funcao para deletar item

--- src/services/cart.js
+++ src/services/cart.js
@@ -9,4 +9,13 @@ async function calculateTotal(userCart) {
   const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
   console.log(`🎁Total: ${result}`);
 }
+
+async function deleteItem(userCart, name) {
+  const index = userCart.findIndex((item) => item.name === name);
+
+  if (index !== -1) {
+    userCart.splice(index, 1);
+  }
+}
+
 export { addItem, calculateTotal, deleteItem, removeItem, displaycart };
